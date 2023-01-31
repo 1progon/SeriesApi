7bdd4b4c557d4e2ae9cc72ef9de7311415912bc3
Anton V <gageviolo@gmail.com>
2023-01-31 16:43:11 +0530

    Add enums

---
+++ SeriesApi/Enums/Movies/MoviesSelector.cs
@@ -0,0 +1,12 @@
+﻿namespace SeriesApi.Enums.Movies;
+
+public enum MoviesSelector
+{
+    Undefined = 0,
+    New = 1,
+    Popular = 2,
+    Soon = 3,
+    Best = 4,
+    Choice = 5,
+    Random = 6,
+}
---
+++ SeriesApi/Enums/Movies/TranslationType.cs
@@ -0,0 +1,8 @@
+﻿namespace SeriesApi.Enums.Movies;
+
+public enum TranslationType
+{
+    Undefined = 0,
+    Voice = 1,
+    Subtitles = 2
+}
