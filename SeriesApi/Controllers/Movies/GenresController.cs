75810128823cc402f239c2ce46894bfaf450d7a2
Anton V <gageviolo@gmail.com>
2023-07-13 21:49:27 +0700

    Add genres controller

---
+++ SeriesApi/Controllers/Movies/GenresController.cs
@@ -0,0 +1,119 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SeriesApi.Data;
+using SeriesApi.Models.Movies;
+
+namespace SeriesApi.Controllers.Movies
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public GenresController(AppDbContext context) => _context = context;
+
+        // GET: api/Genres
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Genre>>> GetGenres()
+        {
+            if (!await _context.Genres.AnyAsync()) return NotFound();
+            return await _context.Genres.ToListAsync();
+        }
+
+        // GET: api/Genres/slug-name
+        [HttpGet("{slug}")]
+        public async Task<ActionResult<Genre>> GetGenre(
+            [FromRoute] string slug,
+            [FromQuery] int limit = 28,
+            [FromQuery] int offset = 0)
+        {
+            var genre = await _context.Genres
+                .Include(g => g.Movies
+                    .Skip(offset)
+                    .Take(limit))
+                .SingleOrDefaultAsync(g => g.Slug == slug);
+
+            if (genre == null) return NotFound();
+
+            return genre;
+        }
+
+        // PUT: api/Genres/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> PutGenre(int id, Genre genre)
+        {
+            if (id != genre.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(genre).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GenreExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Genres
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult<Genre>> PostGenre(Genre genre)
+        {
+            if (_context.Genres == null)
+            {
+                return Problem("Entity set 'AppDbContext.Genres'  is null.");
+            }
+
+            _context.Genres.Add(genre);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGenre", new { id = genre.Id }, genre);
+        }
+
+        // DELETE: api/Genres/5
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> DeleteGenre(int id)
+        {
+            if (_context.Genres == null)
+            {
+                return NotFound();
+            }
+
+            var genre = await _context.Genres.FindAsync(id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            _context.Genres.Remove(genre);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool GenreExists(int id)
+        {
+            return (_context.Genres?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
